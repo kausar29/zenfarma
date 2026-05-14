a25f2db04138168c6d91c02bced64c5052c27a78
Kamruzzaman Kausar <kamruzzamankausar29@gmail.com>
2026-05-14 19:45:20 +0600

    This is first commit

---
+++ envs/dev/backend.tf
@@ -0,0 +1,11 @@
+# backend configuration
+
+terraform {
+  backend "s3" {
+    bucket       = "zen-pharma-terraform-state-dpp-2026"  # Replace with your S3 bucket name
+    key          = "envs/dev/terraform.tfstate"
+    region       = "us-east-1"
+    encrypt      = true
+    use_lockfile = true   # S3 native locking
+  }
+}
