a1d4210f3de3069154cce326421e95aa21c70109
Kamruzzaman Kausar <kamruzzamankausar29@gmail.com>
2026-05-17 00:12:30 +0600

    8th test

--- envs/dev/backend.tf
+++ envs/dev/backend.tf
@@ -6,6 +6,6 @@ terraform {
     key          = "envs/dev/terraform.tfstate"
     region       = "us-east-1"
     encrypt      = true
-    use_lockfile = true   # S3 native locking
+    use_lockfile = false   # S3 native locking
   }
 }
